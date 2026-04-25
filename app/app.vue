0ee8ea747106b4766caa9ba8f5a4b8c42d6b3d17
EdwinRodger <codevaidik@gmail.com>
2026-04-25 18:11:35 +0530

    add devkit and fonts

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="min-h-screen bg-blue-500">
+    <h1 class="text-xl">Hello World</h1>
   </div>
 </template>
+
+<style>
+* {
+  font-family: 'Nunito', sans-serif;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+    vite: {
+        optimizeDeps: {
+            include: [
+                '@vue/devtools-core',
+                '@vue/devtools-kit',
+            ]
+        }
+    },
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
+    modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts'],
+    fonts: {
+        families: [
+            // Automatically resolved if @fontsource/roboto is in your package.json
+            { name: 'Nunito', provider: 'google' },
+        ],
+    }
 })
