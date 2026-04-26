bbc896fa2edbcc5a985d820ee321082779035ffd
EdwinRodger <codevaidik@gmail.com>
2026-04-26 13:31:10 +0530

    add content in about page

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,6 +1,27 @@
 <template>
-    <div>
-        <p class="text-xl container">About Page</p>
+    <div class="bg-slate-600 min-h-screen text-white">
+        <div class="container">
+            <p class="text-2xl py-5">About Noter</p>
+            <p class="">Noter is a simple note-taking app built to explore and learn modern full-stack development using
+                Nuxt and Supabase. The goal of this project is to keep things minimal while focusing on the core
+                fundamentals of building a functional web application.
+                <br>
+                <br>
+                With Noter, you can create, read, update, and delete notes through a clean and straightforward
+                interface.
+                It’s designed without unnecessary complexity, making it easy to understand how data flows between the
+                frontend and backend.
+                <br>
+                <br>
+                Nuxt powers the user interface and routing, while Supabase handles the database and backend services.
+                Together, they provide a fast and developer-friendly environment for building real-world applications.
+                <br>
+                <br>
+                This project serves as a hands-on learning experience, demonstrating how to connect a frontend framework
+                to a
+                backend service, manage state, and implement basic CRUD operations in a practical way.
+            </p>
+        </div>
     </div>
 </template>
 
